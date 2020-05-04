54a74534e0367700d2070c11454a7173e55a5107
Jimmy Millard <millardjimmy@gmail.com>
2020-05-04 22:01:42 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,13 @@
 def unsafe?(speed)
-  if speed >40 || <60
-    return TRUE
-  else return FALSE
+  if speed > 60 
+    true
+  elsif speed < 40
+    true
+  else
+    false
+  end
 end
-
-
 
 def not_safe?(speed)
-	
+  speed < 40 || speed > 60 ? true : false
 end
-	
-
-
